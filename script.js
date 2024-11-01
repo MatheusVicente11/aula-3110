90dd69a8778c4c80d1d8793c86163a83fe5b6be6
MatheusVicente11 <matheusvicente8194k@gmail.com>
2024-10-31 22:24:34 -0300

    AULA 3110

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+elaborew um programa que receba um numero e exiba
+ sua tabuada usando o while 
+*/
+
+var numero = parseInt(prompt("digite um número"));
+
+while (numero == 1) {
+
+}
---
+++ script2.js
@@ -0,0 +1,14 @@
+/*
+elabore um programa que receba uma tarefa a fazer, ao final exiba elas
+*/
+var tarefa = [];
+var i = 0;
+var resposta = 'sim'
+while (resposta == 'sim'){
+ tarefa = prompt("Digite uma tarefa:");
+ i++;
+resposta = prompt("deseja adicionar outra tarefa? se sim digite (sim)")
+
+
+} 
+
